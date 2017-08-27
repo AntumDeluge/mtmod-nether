d541e64790b4c2beb8b17949e26e6b2fcf5c48b8
Jat15 <jinan15@hotmail.com>
2017-08-27 22:01:35 +0200

    Add more compatibilities Stairs, moreblocks
    Modify depend stairs is optional
    Add rack in stairs,and rack, glowstone stairsplus.

--- init.lua
+++ init.lua
@@ -303,6 +303,29 @@ minetest.register_abm({
 	end,
 })
 
+-- Register stair and slab
+
+local function nether_blocks_stairs(nodename, def)	
+	local mod = string.match (nodename,"(.+):")
+	local name = string.match (nodename,":(.+)")
+	minetest.register_node(nodename,def)
+	if minetest.get_modpath("moreblocks") then
+		stairsplus:register_all(
+			mod,
+			name,
+			nodename,
+			def
+		)
+	elseif minetest.get_modpath("stairs") and not( nodename == "nether:glowstone" ) then	
+		stairs.register_stair_and_slab(name,nodename,
+			def.groups,
+			def.tiles,
+			("%s Stair"):format(def.description),
+			("%s Slab"):format(def.description),
+			def.sounds
+		)	
+	end	
+end
 
 -- Nodes
 
@@ -417,7 +440,7 @@ minetest.register_node(":default:obsidian", {
 	end,
 })
 
-minetest.register_node("nether:rack", {
+nether_blocks_stairs("nether:rack", {
 	description = "Netherrack",
 	tiles = {"nether_rack.png"},
 	is_ground_content = true,
@@ -435,7 +458,7 @@ minetest.register_node("nether:sand", {
 	}),
 })
 
-minetest.register_node("nether:glowstone", {
+nether_blocks_stairs("nether:glowstone", {
 	description = "Glowstone",
 	tiles = {"nether_glowstone.png"},
 	is_ground_content = true,
@@ -445,7 +468,7 @@ minetest.register_node("nether:glowstone", {
 	sounds = default.node_sound_glass_defaults(),
 })
 
-minetest.register_node("nether:brick", {
+nether_blocks_stairs("nether:brick", {
 	description = "Nether Brick",
 	tiles = {"nether_brick.png"},
 	is_ground_content = false,
@@ -472,31 +495,6 @@ minetest.register_node("nether:fence_nether_brick", {
 	groups = {cracky = 2, level = 2},
 	sounds = default.node_sound_stone_defaults(),
 })
-
-
--- Register stair and slab
-
-stairs.register_stair_and_slab(
-	"nether_brick",
-	"nether:brick",
-	{cracky = 2, level = 2},
-	{"nether_brick.png"},
-	"nether stair",
-	"nether slab",
-	default.node_sound_stone_defaults()
-)
-
--- StairsPlus
-
-if minetest.get_modpath("moreblocks") then
-	stairsplus:register_all(
-		"nether", "brick", "nether:brick", {
-			description = "Nether Brick",
-			groups = {cracky = 2, level = 2},
-			tiles = {"nether_brick.png"},
-			sounds = default.node_sound_stone_defaults(),
-	})
-end
 
 -- Craftitems
 
